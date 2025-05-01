ec206212a4cdab6d53db76ba3afc95bb496a1b98
bitscon <info@bitscon.net>
2025-05-01 11:35:58 -0500

    test github

--- test_github_file_ops.py
+++ test_github_file_ops.py
@@ -1,13 +1,13 @@
-from src.billy import create_file_in_repo, update_file_in_repo, delete_file_in_repo
+from src.billy import create_file_with_test, update_file_with_test, delete_file_with_commit
 
 # Test creating a file
-create_result = create_file_in_repo("test_file.txt", "This is a test file created by Billy.", "Test creation by Billy")
+create_result = create_file_with_test("test_file.txt", "This is a test file created by Billy.", "Test creation by Billy")
 print("Create Result:", create_result)
 
 # Test updating the file
-update_result = update_file_in_repo("test_file.txt", "This is an updated test file by Billy.", "Test update by Billy")
+update_result = update_file_with_test("test_file.txt", "This is an updated test file by Billy.", "Test update by Billy")
 print("Update Result:", update_result)
 
 # Test deleting the file
-delete_result = delete_file_in_repo("test_file.txt", "Test deletion by Billy")
+delete_result = delete_file_with_commit("test_file.txt", "Test deletion by Billy")
 print("Delete Result:", delete_result)
